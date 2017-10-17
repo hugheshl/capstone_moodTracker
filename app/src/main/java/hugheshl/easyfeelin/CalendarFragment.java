ac05b3eacec6fddbd2d93bc0f69fb49b83162481
Holly Hughes <hugheshl@appstate.edu>
2017-10-17 18:12:05 -0400

    Calendar activity shows monthly counts for each mood, but counts are not yet updated when switch between months

--- app/src/main/java/hugheshl/easyfeelin/CalendarFragment.java
+++ app/src/main/java/hugheshl/easyfeelin/CalendarFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -31,6 +32,23 @@ public class CalendarFragment extends DialogFragment {
     private DatePicker mDatePicker;
 
     private TextView mTextView;
+    private TextView mMood1Count;
+    private TextView mMood2Count;
+    private TextView mMood3Count;
+    private TextView mMood4Count;
+    private TextView mMood5Count;
+
+    private int m1Count = 0;
+    private int m2Count = 0;
+    private int m3Count = 0;
+    private int m4Count = 0;
+    private int m5Count = 0;
+
+    private ImageView face1ImageView;
+    private ImageView face2ImageView;
+    private ImageView face3ImageView;
+    private ImageView face4ImageView;
+    private ImageView face5ImageView;
 
     public static CalendarFragment newInstance(Date date) {
         CalendarFragment calendarfragment = new CalendarFragment();
@@ -43,6 +61,8 @@ public class CalendarFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
 
@@ -73,18 +93,52 @@ public class CalendarFragment extends DialogFragment {
         mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
         mDatePicker.init(year, month, day, null);
 
-        for(int i = 0; i < assigns.size(); i++)
-        {
-            //get total for each mood per month,
-            //color-code days
-            for(Assign a : assigns) {
-                if(a.getMood() == 1) {
-                    //color matching day to green
-                    
-                }
-            }
+        for(Assign a : assigns) {
 
+            if(a.getMood() == 1 && month == a.getDate().getMonth()) {
+                m1Count++;
+                //color matching day to green
+
+            }
+            else if(a.getMood() == 2 && month == a.getDate().getMonth()) {
+                m2Count++;
+                //color change day to purple
+
+            }
+            else if(a.getMood() == 3 && month == a.getDate().getMonth()) {
+                m3Count++;
+                //color change day to blue
+
+            }
+            else if(a.getMood() == 4 && month == a.getDate().getMonth()) {
+                m4Count++;
+                //color change day or orange
+
+            }
+            else if(a.getMood() == 5 && month == a.getDate().getMonth()) {
+                m5Count++;
+                //color change day to red
+
+            }
         }
+
+        face1ImageView = (ImageView) v.findViewById(R.id.imageViewFace1);
+        face2ImageView = (ImageView) v.findViewById(R.id.imageViewFace2);
+        face3ImageView = (ImageView) v.findViewById(R.id.imageViewFace3);
+        face4ImageView = (ImageView) v.findViewById(R.id.imageViewFace4);
+        face5ImageView = (ImageView) v.findViewById(R.id.imageViewFace5);
+
+        mMood1Count = (TextView) view.findViewById(R.id.mood1Count);
+        mMood2Count = (TextView) view.findViewById(R.id.mood2Count);
+        mMood3Count = (TextView) view.findViewById(R.id.mood3Count);
+        mMood4Count = (TextView) view.findViewById(R.id.mood4Count);
+        mMood5Count = (TextView) view.findViewById(R.id.mood5Count);
+
+        mMood1Count.setText("Great: " + m1Count);
+        mMood2Count.setText("Good: " + m2Count);
+        mMood3Count.setText("Ok: " + m3Count);
+        mMood4Count.setText("Bad: " + m4Count);
+        mMood5Count.setText("Horrible: " + m5Count);
 
         return view;
     }
