db3488c93a6e3f9d8da7b01c02c9724f50c9d88a
Holly Hughes <hugheshl@appstate.edu>
2017-10-29 10:59:59 -0400

    Working on chart screen. Correlations in progress, has bug

--- app/src/main/java/hugheshl/easyfeelin/ChartFragment.java
+++ app/src/main/java/hugheshl/easyfeelin/ChartFragment.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -22,6 +25,7 @@ import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -35,6 +39,7 @@ public class ChartFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
 
     private TextView mTextView;
+    private TextView mActivityCorrelations;
 
     public static ChartFragment newInstance() {
         ChartFragment chartfragment = new ChartFragment();
@@ -59,10 +64,9 @@ public class ChartFragment extends DialogFragment {
         mTextView.setText("Total number of entries: " + mytext);
 
         GraphView graph = (GraphView) view.findViewById(R.id.graph);
+
         DataPoint[] data = new DataPoint[assigns.size()];
-
         int count = 0;
-
         for(Assign a : assigns) {
             data[count] = new DataPoint(count, a.getMood());
             count++;
@@ -70,6 +74,68 @@ public class ChartFragment extends DialogFragment {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
         graph.addSeries(series);
 
+        graph.setTitle("Mood Chart");
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(12);
+        series.setDrawBackground(true);
+
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(15);
+
+        graph.getGridLabelRenderer().setNumHorizontalLabels(15);
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Mood");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("2 Week Window");
+        graph.getGridLabelRenderer().setHorizontalLabelsAngle(20);
+
+        //----------------------------------------------------------------------
+        //another set of data points to color code the graph (i.e. green dot in the chart for a great day)
+        DataPoint[] data_1 = new DataPoint[assigns.size()];
+        int count2 = 0;
+        for(Assign a : assigns) {
+            if(a.getMood() == 1) {
+                data_1[count2] = new DataPoint(count2, a.getMood());
+                count2++;
+            }
+        }
+        PointsGraphSeries<DataPoint> series2 = new PointsGraphSeries<>(data_1);
+        //graph.addSeries(series2);                                                  //ERROR
+        series2.setShape(PointsGraphSeries.Shape.POINT);
+        series2.setColor(Color.GREEN);
+        //----------------------------------------------------------------------
+
+        //Correlate activities to mood or vice versa - below line graph
+        String[] activityArray = {"Working | ", "Relaxing | ", "Hanging out with friends | ", "Date | ",
+                "Sports | ", "Partying | ", "Watching TV | ", "Reading | ", "Gaming | ", "Shopping | ", "Travel | ",
+                "Good meal | ", "Cleaning | ", "Napping | ", "Homework | "};
+        String top1 = "";
+
+        int[] greatMoodActivities = new int[14];
+        for(Assign a : assigns) {
+            if(a.getMood() == 1) {
+                //parse activity string data - put into array and keep count of great days for each activity
+                String activities = a.getLogs();
+                char[] actArray = new char[14];
+                activities.getChars(0, 13, actArray, 0); // NULL POINTER EXCEPTION
+                for(int i = 0; i < actArray.length; i++) {
+                    if(actArray[i] == '1') {
+                        greatMoodActivities[i]++;
+                    }
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < greatMoodActivities.length; i++) {
+            if (greatMoodActivities[i] > max) {
+                max = i;
+            }
+        }
+        top1 = activityArray[max];
+
+        mActivityCorrelations = (TextView) view.findViewById(R.id.textView2);
+        mActivityCorrelations.setText("Top Activity Correlated with a Great Mood: " + top1);
 
 
         if (savedInstanceState != null) {
